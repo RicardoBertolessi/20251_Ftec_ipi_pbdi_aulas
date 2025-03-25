9aa65c2e1105a008def74f084d9e78655d556a1e
Ricardo Galdino <rgaldino510@gmail.com>
2025-03-25 11:31:38 -0300

    Ap10: encerra o codigo 2.4.4

---
+++ ap10_estruturas_de_repeticao/scripit.sql
@@ -0,0 +1,59 @@
+DO $$
+DECLARE
+    contador INT := 0;
+BEGIN
+    LOOP
+        contador := contador + 1;
+        EXIT WHEN contador > 100;
+
+        IF contador % 7 = 0 THEN
+            CONTINUE;
+        END IF;
+
+        CONTINUE WHEN contador % 11 = 0;
+
+        RAISE NOTICE '%', contador;
+    END LOOP;
+END
+$$
+
+
+
+
+-- DO $$
+
+-- DECLARE
+--     contador INT := 1;
+-- BEGIN
+--     LOOP
+--         RAISE NOTICE '%', contador;
+--         contador := contador +1;
+--         EXIT WHEN contador > 10;
+--     END LOOP;
+-- END;
+-- $$
+
+
+-- DO $$
+-- DECLARE
+--     contador INT := 1;
+-- BEGIN
+--     LOOP
+--         RAISE NOTICE '%', contador;
+--         contador := contador + 1;
+--         IF contador > 10 THEN
+--             EXIT;
+--         END IF;
+--     END LOOP;
+-- END;
+-- $$
+
+
+
+-- DO $$
+-- BEGIN
+--     LOOP
+--         RAISE NOTICE 'Um loop simples...';
+--     END LOOP;
+-- END;
+-- $$
